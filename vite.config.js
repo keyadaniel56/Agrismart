2f66697bbae95bda23caa93378bcaadfcd61f52f
danikeya <keyadaniel56@gmail.com>
2026-02-26 12:19:15 +0300

    Configure Vite for HTTPS serving with self-signed certificate

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  server: {
+    port: 3000,
+    https: {
+      key: fs.readFileSync('.ssl/key.pem'),
+      cert: fs.readFileSync('.ssl/cert.pem'),
+    }
+  }
 })
