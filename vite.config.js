f8a68d9f0c16660807ae59fd03f2e95f40a2d855
Haji Ismael Ibrahim <hajisml@outlook.com>
2026-02-26 14:34:43 +0300

    fix: make https conditional to avoid error when .ssl directory is missing

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
+import path from 'path'
+
+const hasCert = fs.existsSync('.ssl/key.pem') && fs.existsSync('.ssl/cert.pem')
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     host: true,
-    https: {
+    https: hasCert ? {
       key: fs.readFileSync('.ssl/key.pem'),
       cert: fs.readFileSync('.ssl/cert.pem'),
-    }
+    } : false
   }
 })
